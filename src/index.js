0347f9eb1e71cda7b4421b037ea7afdb32a1b341
Alamin <alaminprogramerr@gmail.com>
2021-12-18 00:15:27 +0600

    done

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { MoralisProvider } from "react-moralis";
+import "./index.css";
+import QuickStart from "components/QuickStart";
+
+/** Get your free Moralis Account https://moralis.io/ */
+
+// const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+// const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+const APP_ID = "6L7OOVrWR9gWMG40bKYkpquMCjNDxklIjAmxxWWT"
+const SERVER_URL = "https://qdorlbfzbadw.usemoralis.com:2053/server"
+
+const Application = () => {
+  const isServerInfo = APP_ID && SERVER_URL ? true : false;
+  //Validate
+  if (!APP_ID || !SERVER_URL) throw new Error("Missing Moralis Application ID or Server URL. Make sure to set your .env file.");
+  if (isServerInfo)
+    return (
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+        <App isServerInfo />
+      </MoralisProvider>
+    );
+  else {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <QuickStart />
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(
+  // <React.StrictMode>
+  <Application />,
+  // </React.StrictMode>,
+  document.getElementById("root")
+);
