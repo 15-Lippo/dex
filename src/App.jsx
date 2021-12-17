0464c0e71afaf6bcad99feed8cfef24bbbc35215
Alamin <alaminprogramerr@gmail.com>
2021-12-18 00:22:42 +0600

    rm  unsd

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,10 @@ import { Layout, Tabs } from "antd";
 import "antd/dist/antd.css";
 import NativeBalance from "components/NativeBalance";
 import "./style.css";
-import QuickStart from "components/QuickStart";
 import Contract from "components/Contract/Contract";
-import Text from "antd/lib/typography/Text";
 import Ramper from "components/Ramper";
 import MenuItems from "./components/MenuItems";
-const { Header, Footer } = Layout;
+const { Header } = Layout;
 
 const styles = {
   content: {
